687d1c7c888b094d5d29859cc380916cac3d344c
Alexandra RIBEIRO <aribeiro@e3r2p5.42.fr>
2017-08-21 20:10:30 +0200

    backup reader() ok

--- Lead.cpp
+++ Lead.cpp
@@ -6,7 +6,7 @@
 /*   By: aribeiro <aribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/17 18:43:11 by aribeiro          #+#    #+#             */
-/*   Updated: 2017/08/21 19:16:28 by aribeiro         ###   ########.fr       */
+/*   Updated: 2017/08/21 20:08:58 by aribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,16 +37,25 @@ void		Lead::reader(int ac, char **av) {
 		_arg = av[2];
 		std::cout << _arg;
 	}
+	else if (ac == 3 && _arg.compare(av[2]) == 0) {
+		_arg = av[1];
+		std::cout << _arg;
+	}
 	else if (ac == 2) {
 		_arg = av[1];
 		std::cout << _arg;
 	}
+	else if (ac > 2) {
+		throw BaseException("=> Error too many arguments.");
+	}
 	else {
 		std::string msg;
-		msg.append(GREEN);
-		msg.append("Usage :\n\t-v: verbose\n\tThis program takes an argument in parameter,");
-		msg.append("a polynomial equation (simple or quadratic)\n\t(ex: 5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0).");
 		msg.append(NORMAL);
+		msg.append("\n\tUSAGE\n\t------------\n\t");
+		msg.append("This program takes an argument in parameter, a polynomial equation (simple or quadratic)");
+		msg.append("\n\tex: 5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0\n\n\t");
+		msg.append("-v : verbose\n");
+
 		throw BaseException(msg);
 	}
 }
